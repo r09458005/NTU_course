f8d4a069954b20be836a32342066aa9f4918b12c
r09458005 <81867916+r09458005@users.noreply.github.com>
2022-06-23 02:31:28 +0800

    Delete test1135.py

--- test1135.py
+++
@@ -1,8 +0,0 @@
-import math
-
-input_array = input().split()
-n = len(input_array)
-median = (sum(input_array[n//2-1:n//2+1])/2.0, lst[n//2])[n % 2] if n else None
-ans = math.ceil(median)
-print('%.f'%ans)
-
